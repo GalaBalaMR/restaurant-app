0c6c05604d9b559fdc26a83b3dbbafd733520da8
GalaBalaMR <matus.recka@gmail.com>
2022-09-07 08:50:48 +0200

    Add other seeder to run function

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-        $this->call(AdminSeeder::class);
+        $this->call([AdminSeeder::class,
+                     ContentSeeder::class,
+                     CategorySeeder::class,
+                     MenuSeeder::class,
+                     RoleAndPermision::class
+                    ]);
     }
 }
